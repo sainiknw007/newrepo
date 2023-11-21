2a7acdf08936ae9240a88b7ff9afa466e1bfbd4f
sainiknw007 <sainiknw007@gmail.com>
2023-11-21 08:56:31 +0530

    this is commit1 in local repo

---
+++ A-Script1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script is to read input from the user/console
+echo "value of a"
+read a
+echo "value of b"
+read b
+echo "Hello value of a is $a and value of b is $b"
+echo "we are performing this practical for batch-20"
