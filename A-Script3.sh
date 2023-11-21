4b22bf936a88df8720cf1a28e67e6b436a7737c4
sainiknw007 <sainiknw007@gmail.com>
2023-11-21 09:29:03 +0530

    this is second file

---
+++ A-Script3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# this is to check if the shellscript picks up env variables
+echo "This is error script"
+echo "This is my system path $PATH"
+
+#Set a JDK environment variable.
+export JDK_HOME=/bin/jdk
+echo "my new JDK Home is=$JDK_HOME"
